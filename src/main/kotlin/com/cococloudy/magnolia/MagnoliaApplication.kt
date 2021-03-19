fa789fd0ca875efb6f0cb486205261e01427c559
YoungHoon <nobel6018@gmail.com>
2021-03-20 02:52:32 +0900

    feat: initialize

---
+++ src/main/kotlin/com/cococloudy/magnolia/MagnoliaApplication.kt
@@ -0,0 +1,11 @@
+package com.cococloudy.magnolia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MagnoliaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MagnoliaApplication>(*args)
+}
---
+++ src/test/kotlin/com/cococloudy/magnolia/MagnoliaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cococloudy.magnolia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MagnoliaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
