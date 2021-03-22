f96c48e002ba67bd4e80efeab42ca6e40dd2c841
YoungHoon <nobel6018@gmail.com>
2021-03-22 13:10:02 +0900

    fix: write third party library on README.md, refac and update codes

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ repositories {
 }
 
 dependencies {
+    val jsonwebtokenVersion = "0.11.2"
+    val openApiVersion = "1.5.6"
+    val querydslVersion = "4.4.0"
+
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
@@ -34,14 +38,12 @@ dependencies {
     implementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
-    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
-
-    val querydslVersion = "4.4.0"
+    implementation("org.springdoc:springdoc-openapi-ui:$openApiVersion")
+    implementation("org.springdoc:springdoc-openapi-kotlin:$openApiVersion")
     implementation("com.querydsl:querydsl-jpa:$querydslVersion")
     kapt("com.querydsl:querydsl-apt:$querydslVersion:jpa")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
